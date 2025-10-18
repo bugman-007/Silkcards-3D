04a1c97448459f977b266a95bfee1a27949f7f5c
bugman-007 <terryximons@gmail.com>
2025-10-18 20:23:38 +0000

    add some small env change

---
+++ parser-service/scripts/runtime/job.jsx
@@ -0,0 +1,5 @@
+var __JOB = {
+  input: "C:/Users/Administrator/Desktop/Silkcards-3D/parser-service/jobs/incoming/3026dd63-cb0e-448d-ad3f-3c9257c06c59__Tiger Queen - 1 page - Options in Layers - no clipping mask.ai",
+  output: "C:/Users/Administrator/Desktop/Silkcards-3D/parser-service/jobs/working/3026dd63-cb0e-448d-ad3f-3c9257c06c59",
+  job_id: "3026dd63-cb0e-448d-ad3f-3c9257c06c59"
+};
